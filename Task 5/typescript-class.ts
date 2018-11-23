efc85327050d4f234e0842ace3092aaf4e578df7
lichmaniuk <pavel_lichmaniuk@epam.com>
2018-11-23 16:25:33 +0300

    Task 5 - Fixed tslint mistaces

--- Task 5/typescript-class.ts
+++ Task 5/typescript-class.ts
@@ -1,45 +1,48 @@
 class Lada {
-    private _isOn: boolean = false;
+    public model: string;
+    public power: number;
+    private isOn: boolean = false;
 
-    constructor(model: string, power: number) { }
-
-    start(): void {
-        this._isOn = true;
+    constructor(model: string, power: number) {
+        this.model = model;
+        this.power = power;
     }
 
-    stop(): void {
-        this._isOn = false;
+    public start(): void {
+        this.isOn = true;
     }
-};
 
+    public stop(): void {
+        this.isOn = false;
+    }
+}
 
 class Mercedes extends Lada {
-    private _isHeadlightsOn: boolean = false;
+    private isHeadlightsOn: boolean = false;
 
     constructor(model: string, power: number) {
         super(model, power);
-        
     }
 
-    _headlightsOn(): void {
-        this._isHeadlightsOn = true;
-    }
-
-    _headlightsOff(): void {
-        this._isHeadlightsOn = false;
-    }
-
-    start(): void {
+    public start(): void {
         super.start();
-        this._headlightsOn();
+        this.headlightsOn();
         console.log("Mercedes is ready to go");
     }
 
-    stop(): void {
+    public stop(): void {
         super.stop();
-        this._headlightsOff();
+        this.headlightsOff();
         console.log("Mercedes is stoped");
     }
-};
 
-new Lada().
+    private headlightsOn(): void {
+        this.isHeadlightsOn = true;
+    }
+
+    private headlightsOff(): void {
+        this.isHeadlightsOn = false;
+    }
+}
+
+let merin = new Mercedes("W222", 300);
